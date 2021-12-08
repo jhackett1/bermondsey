b2fb9173f2900d318193fdaf5413d52b4bbaa05f
jaye <hello@jayehackett.com>
2021-12-08 13:59:14 +0000

    add sqlite

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const noble = require("@abandonware/noble")
+const sqlite3 = require("sqlite3")
+const { open } = require("sqlite")
+
+// this is a top-level await
+;(async () => {
+  // open the database
+  const db = await open({
+    filename: "/tmp/database.db",
+    driver: sqlite3.Database,
+  })
+  // set up a table
+  await db.exec("CREATE TABLE friends (uuid TEXT)")
+})()
+
+// https://www.npmjs.com/package/@abandonware/noble/v/1.9.2-8
 
 noble.on("stateChange", async state => {
   if (state === "poweredOn") {
@@ -7,7 +22,7 @@ noble.on("stateChange", async state => {
 })
 
 noble.on("discover", async peripheral => {
-  await noble.stopScanningAsync()
+  // await noble.stopScanningAsync()s
   await peripheral.connectAsync()
   const { characteristics } =
     await peripheral.discoverSomeServicesAndCharacteristicsAsync(
@@ -21,5 +36,5 @@ noble.on("discover", async peripheral => {
   )
 
   await peripheral.disconnectAsync()
-  process.exit(0)
+  // process.exit(0)
 })
